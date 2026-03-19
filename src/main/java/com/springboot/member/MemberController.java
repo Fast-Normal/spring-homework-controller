4d440f0d48a89b31755a3e7fc2e9b38e6b6a284f
Yunsun <justina7182@gmail.com>
2026-03-19 18:37:44 +0900

    feat:Controller 실습 coffeeController,memberController 수정

--- src/main/java/com/springboot/member/MemberController.java
+++ src/main/java/com/springboot/member/MemberController.java
@@ -1,8 +1,10 @@
 package com.springboot.member;
 
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,9 +25,55 @@ public class MemberController {
 
         members.put(memberId, member1);
     }
+    @GetMapping("/{member-id}")
+    public String getMember(@PathVariable("member-id") long memberId, Model model) {
+        System.out.println("# memberId: " + memberId);
+        model.addAttribute("memberId", memberId);
+        return "memberDetail"; // templates/memberDetail.html
+    }
+
+    @GetMapping // 별도 URI 설정이 없으므로 클래스 레벨 경로인 /v1/members 요청을 처리
+    public String getMembers(Model model) {
+        System.out.println("# get Members");
+        return "memberList"; //templates.memberList.html
+    }
+
+    @PostMapping
+    public String postMember(@RequestParam("email") String email,
+                             @RequestParam("name") String name,
+                             @RequestParam("phone") String phone,
+                             Model model) {
+        System.out.println("# email: " + email);
+        System.out.println("# name: " + name);
+        System.out.println("# phone: " + phone);
+
+        // model: view(HTML)로 데이터를 전달하기 위해 스프링이 제공하는 바구니 객체
+        model.addAttribute("email", email);
+        model.addAttribute("name", name);
+        model.addAttribute("phone", phone);
+
+        return "memberResult"; // templates/memberResult.html
+    }
 
     //---------------- 여기서 부터 아래에 코드를 구현하세요! --------------------//
     // 1. 회원 정보 수정을 위한 핸들러 메서드 구현
+    @PatchMapping("/{memberId}")
+    public ResponseEntity<Map<String, Object>> patchMember(
+            @PathVariable long memberId,
+            @RequestParam String phone) {
+
+        Map<String, Object> member = members.get(memberId);
+        member.put("phone", phone);
+
+        return new ResponseEntity<>(member, HttpStatus.OK);
+    }
+
     // 2. 회원 정보 삭제를 위한 핸들러 메서드 구현
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity<Object> deleteMember(
+            @PathVariable long memberId) {
+
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
 
 }
